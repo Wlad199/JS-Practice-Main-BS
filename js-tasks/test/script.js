0ac85e85a0b7968599b19113e31fb049ac748093
Wlad199 <habilis199@mail.ru>
2024-05-03 11:51:32 +0300

    add solutions

---
+++ js-tasks/test/script.js
@@ -0,0 +1,6 @@
+const text = document.querySelector('.text')
+const list = document.querySelector('.list')
+const bt01 = document.getElementById('bt-01')
+const bt02 = document.getElementById('bt-02')
+
+console.log(document.documentElement.clientWidth)
