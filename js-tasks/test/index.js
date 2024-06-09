acff4ec612325ccc2d3b607b667e08fc719fc380
Wlad199 <habilis199@mail.ru>
2024-06-09 11:19:05 +0300

    change main girectory

--- js-tasks/test/index.js
+++
@@ -1,6 +0,0 @@
-function frame(elem, dir) {
-	if (dir === 1 /* Directions.LEFT */) {
-		console.log(elem);
-	}
-}
-console.log(frame('yes', 1 /* Directions.LEFT */));
---
+++ sandbox/test/index.js
@@ -0,0 +1,6 @@
+function frame(elem, dir) {
+	if (dir === 1) {
+		console.log(elem);
+	}
+}
+console.log(frame('yes', 1));
